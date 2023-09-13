5034e2112205493e9e7c9b0cf39d7eebdade3e4f
SiriusMklain <sergeimakarenko5991@gmail.com>
2023-09-13 21:52:30 +0300

    sdfdsf

--- main.py
+++ main.py
@@ -109,16 +109,18 @@ def change_colum():
     pd.set_option('display.width', 500)
     pd.set_option('display.max_columns', None)
     pd.set_option('display.max_rows', None)
-    print(new_df.head(10))
     out_df = pd.read_excel('result_colum_category.xlsx')
     out_df = out_df.drop(columns=["HorsePowers"])
     new_df = new_df.merge(out_df, how='left', on=["Name", "VM", "TypeName"])
+    new_df = new_df.drop_duplicates(subset=['МОДЕЛЬ', 'КОД ДВИГАТЕЛЯ', 'Мощность Л.С', "Year"])
+
     new_df = new_df.drop(columns=["Name"])
     new_df = new_df.drop(columns=["VM"])
     new_df = new_df.drop(columns=["TypeName"])
     new_df = new_df.drop(columns=["Engines"])
+    new_df.rename(columns={'Фильтр, воздух во внутренном пространстве': 'Воздушный фильтр'}, inplace=True)
     print(new_df.head(10))
-    # new_df.to_excel("res.xlsx", index=False)
+    new_df.to_excel("res.xlsx", index=False)
 
 
     #     # приводим дату к нужному формату
